33fb88370c73f3d0e248024922faeaaa19572192
Kay <example@mail.com>
2024-10-10 16:31:25 +0200

    First setup for the project. Wiped all css, setup header with nav. Still needs some finetuning.

---
+++ election-frontend/src/components/ECHeader.vue
@@ -0,0 +1,94 @@
+<script setup>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
+
+<template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <div class="header-top">
+    <div class="container">
+      <img alt="Vue logo" src="../assets/logo-ec-big.svg">
+    </div>
+  </div>
+
+  <div class="topnav" id="myTopnav">
+    <div class="containernav">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/test">Test</RouterLink>
+      <a href="javascript:void(0);" class="icon" @click="myFunction">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.containernav {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #004494;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.topnav a:hover {
+  background-color: #B9C5E9;
+  color: #26324B;
+}
+
+.topnav a.router-link-active {
+  background-color: #B9C5E9;
+  color: white;
+}
+
+.topnav .icon {
+  display: none;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a:not(:first-child) {
+    display: none;
+  }
+
+  .topnav a.icon {
+    float: right;
+    display: block;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .topnav.responsive {
+    position: relative;
+  }
+
+  .topnav.responsive .icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+
+  .topnav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+}
+</style>
--- election-frontend/src/views/TestView.vue
+++ election-frontend/src/views/TestView.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="about">
-    <h1>Test 123</h1>
+    <h1>This is the test page</h1>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
+
 <script setup lang="ts">
 </script>
