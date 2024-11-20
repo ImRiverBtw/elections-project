3b98545b8e1a274e86a208c4fa634a48afe8431c
Dave <dave.de.wekker@hva.nl>
2024-11-20 16:52:43 +0100

    Bug fixes

--- election-frontend/src/components/ElectionResults/ElectionResultsMapComponent.vue
+++ election-frontend/src/components/ElectionResults/ElectionResultsMapComponent.vue
@@ -15,11 +15,12 @@ import Fuse from 'fuse.js';
 export default {
   data() {
     return {
+      map: null,
       geojson: null,
+      rawGeoJson: null,
       gemeentenVotesMap: {},
       gekozenGemeentens: new Set(),
-      fuse: null,
-      data: null
+      fuse: null
     };
   },
   created() {
@@ -45,28 +46,103 @@ export default {
           };
 
           this.fuse = new Fuse(normalizedGemeenten, fuseOptions);
+          this.addGeoJsonToMap(); // Update de kaart
         })
         .catch(error => {
           console.error('Error fetching election results:', error);
+          this.fuse = new Fuse([], {}); // Lege Fuse-instantie voor veiligheid
         });
   },
   mounted() {
     let bounds = [[50.5, 3.2], [53.7, 7.2]];
 
-    let map = L.map('map', {
+    this.map = L.map('map', {
       center: [52.1, 5.3],
       zoom: 7,
       minZoom: 7
     });
 
-    map.setMaxBounds(bounds);
+    this.map.setMaxBounds(bounds);
 
     // Voeg OpenStreetMap tegels toe
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(this.map);
 
-    function getColor(party) {
+    // Haal de GeoJSON data op
+    fetch('https://www.webuildinternet.com/articles/2015-07-19-geojson-data-of-the-netherlands/townships.geojson')
+        .then(response => response.json())
+        .then(data => {
+          this.rawGeoJson = data;
+          this.addGeoJsonToMap();
+        })
+        .catch(error => console.error('Error fetching GeoJSON data:', error));
+  },
+  methods: {
+    addGeoJsonToMap() {
+      if (!this.rawGeoJson || !this.fuse || Object.keys(this.gemeentenVotesMap).length === 0) {
+        return;
+      }
+
+      const style = (feature) => {
+        let gemeenteNaam = feature.properties.name;
+        let winningParty = this.gemeentenVotesMap[gemeenteNaam] || this.gemeentenVotesMap[this.normalize(gemeenteNaam)];
+
+        if (!winningParty && !this.gekozenGemeentens.has(gemeenteNaam)) {
+          const fuzzyResult = this.fuse.search(this.normalize(gemeenteNaam));
+          if (fuzzyResult.length > 0) {
+            const bestMatch = fuzzyResult[0].item.original;
+            winningParty = this.gemeentenVotesMap[bestMatch];
+          }
+        }
+
+        feature.properties.winning_party = winningParty || 'Onbekend';
+
+        return {
+          fillColor: this.getColor(winningParty),
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+        };
+      };
+
+      const highlightFeature = (e) => {
+        let layer = e.target;
+        layer.setStyle({
+          weight: 5,
+          color: '#666',
+          dashArray: '',
+          fillOpacity: 0.9
+        });
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+          layer.bringToFront();
+        }
+      };
+
+      const resetHighlight = (e) => {
+        this.geojson.resetStyle(e.target);
+      };
+
+      const onEachFeature = (feature, layer) => {
+        layer.on({
+          mouseover: highlightFeature,
+          mouseout: resetHighlight
+        });
+        layer.bindPopup('<strong>' + feature.properties.name + '</strong><br>Grootste partij: ' + feature.properties.winning_party);
+      };
+
+      if (this.geojson) {
+        this.map.removeLayer(this.geojson);
+      }
+
+      this.geojson = L.geoJson(this.rawGeoJson, {
+        style: style,
+        onEachFeature: onEachFeature
+      }).addTo(this.map);
+    },
+    getColor(party) {
       return party === 'NSC' ? '#FFDD00' :
           party === 'PVV' ? '#1E90FF' :
               party === 'GLPVDA' ? '#FF0000' :
@@ -77,94 +153,22 @@ export default {
                                   party === 'CDA' ? '#006400' :
                                       party === 'FVD' ? '#800020' :
                                           party === 'PVDD' ? '#00A86B' :
-                              '#CCCCCC';  // Grijs als default
-    }
-
-    let directMatch = [];
-    let noMatch = [];
-    let fuzzyMatch = [];
-
-    const style = (feature) => {
-      let gemeenteNaam = feature.properties.name;
-
-
-      let winningParty = this.gemeentenVotesMap[gemeenteNaam] || this.gemeentenVotesMap[this.normalize(gemeenteNaam)];
-
-      if (!winningParty && !this.gekozenGemeentens.has(gemeenteNaam)) {
-        const fuzzyResult = this.fuse.search(this.normalize(gemeenteNaam));
-
-        if (fuzzyResult.length > 0) {
-          const bestMatch = fuzzyResult[0].item.original;
-          winningParty = this.gemeentenVotesMap[bestMatch];
-        }
-      }
-
-      feature.properties.winning_party = winningParty || 'Onbekend';
-
-      return {
-        fillColor: getColor(winningParty),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-      };
-    };
-
-    console.log("direct match ", directMatch);
-    console.log("no match ", noMatch);
-    console.log("fuzzy match", fuzzyMatch);
-
-    const highlightFeature = (e) => {
-      let layer = e.target;
-      layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.9
-      });
-      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-      }
-    };
-
-    const resetHighlight = (e) => {
-      this.geojson.resetStyle(e.target);
-    };
-
-    const onEachFeature = (feature, layer) => {
-      layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight
-      });
-      layer.bindPopup('<strong>' + feature.properties.name + '</strong><br>Grootste partij: ' + feature.properties.winning_party);
-    };
-
-    // Haal de GeoJSON data op en voeg deze toe aan de kaart
-    fetch('https://www.webuildinternet.com/articles/2015-07-19-geojson-data-of-the-netherlands/townships.geojson')
-        .then(response => response.json())
-        .then(data => {
-          this.geojson = L.geoJson(data, {
-            style: style,
-            onEachFeature: onEachFeature
-          }).addTo(map);
-        })
-        .catch(error => console.error('Error fetching GeoJSON data:', error));
-  },
-  methods: {
-    // Hulpmethode om een array te schudden
-    shuffleArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
+                                              '#CCCCCC'; // Grijs als default
     },
     normalize(name) {
-      const normalized = name.toLowerCase().replace(/[^a-z0-9]/g, '')
-      if (normalized) {
-        return normalized;
-      }
+      return name.toLowerCase().replace(/[^a-z0-9]/g, '');
+    }
+  },
+  watch: {
+    fuse: 'addGeoJsonToMap',
+    gemeentenVotesMap: 'addGeoJsonToMap'
+  },
+  beforeDestroy() {
+    if (this.geojson) {
+      this.map.removeLayer(this.geojson);
+    }
+    if (this.map) {
+      this.map.remove();
     }
   }
 };
