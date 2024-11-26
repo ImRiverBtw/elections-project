4512816d40281160394aa53356411af58feac831
Dave <dave.de.wekker@hva.nl>
2024-11-26 16:44:24 +0100

    Login error

--- election-frontend/src/components/LoginPopup.vue
+++ election-frontend/src/components/LoginPopup.vue
@@ -9,6 +9,7 @@
       <div class="inputBox">
         <div class="inputText email">Email-addres</div>
         <input class="inputField email" v-model="loginEmail" placeholder="Email-addres" />
+        <small v-if="emailError" class="text-error">{{ emailError }}</small>
 
         <div class="inputText password">Wachtwoord</div>
         <input class="inputField password" v-model="loginPassword" placeholder="Wachtwoord" />
@@ -33,6 +34,7 @@ export default {
     return {
       loginEmail: '',
       loginPassword: '',
+      emailError: null,
     };
   },
   methods: {
@@ -40,14 +42,26 @@ export default {
       this.$emit('close');
     },
     async submit() {
+      this.emailError = null;
+
       try {
-        const loginResponse  = await login(this.loginEmail, this.loginPassword);
-        console.log("Ingelogd succesful: ", loginResponse);
+        console.log("Ingelogd succesful");
         this.close();
       } catch (error) {
-        console.log('Login mislukt: ' + error);
+        if (error.message.includes('Invalid email or password')) {
+          this.emailError = 'E-mailadres of wachtwoord is onjuist.';
+        } else {
+          this.emailError = 'Er is een probleem opgetreden. Probeer het later opnieuw.';
+        }
       }
     },
   },
 };
 </script>
+
+<style>
+.text-error {
+  font-size: 12px;
+  color: red;
+}
+</style>
