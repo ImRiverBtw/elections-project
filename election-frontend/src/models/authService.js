5ba7f1b760aa1a7ba5f641a7e2c61e90868d8975
Dave <dave.de.wekker@hva.nl>
2024-11-27 16:05:40 +0100

    Aanpassing van merge zodat registeren werkt

--- election-frontend/src/models/authService.js
+++ election-frontend/src/models/authService.js
@@ -1,10 +1,11 @@
 const API_BASE_URL = 'http://localhost:8080/auth';
+const API_BASE_URL_USERDATA = 'http://localhost:8080/userdata';
 
 export async function login(email, password) {
     const loginData = { email, password };
 
     try {
-        const response = await fetch(`${API_BASE_URL}/login`, {
+        const response = await fetch(`${API_BASE_URL_USERDATA}/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(loginData),
@@ -25,7 +26,7 @@ export async function register(username, email, password) {
     const registerData = { username, email, password };
 
     try {
-        const response = await fetch(`${API_BASE_URL}/register`, {
+        const response = await fetch(`${API_BASE_URL_USERDATA}/register`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(registerData),
