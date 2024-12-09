27ec9563ce14b256711aa2b906799c5bbe519d3f
Robin Hoogendijk <robin.hoogendijk@hva.nl>
2024-12-09 09:01:07 +0100

    made database persistant again and reversed a previous change that broke things

--- election-backend/src/main/java/com/election/electionbackend/security/JWTRequestFilter.java
+++ election-backend/src/main/java/com/election/electionbackend/security/JWTRequestFilter.java
@@ -12,11 +12,10 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
-@RequiredArgsConstructor
+
 public class JWTRequestFilter extends OncePerRequestFilter {
 
-    private final APIConfig apiConfig;
+    private APIConfig apiConfig;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
